bb01ac975cdf7ba70d998f3764391b531c1d0ab4
davidlerner26 <david.lerner@hotmail.com>
2023-12-19 12:56:49 -0300

    Set if user is logged in based on firebase auth currentUser value

--- src/App.vue
+++ src/App.vue
@@ -272,11 +272,22 @@
 <script>
 import AppHeader from '@/components/Header.vue'
 import AppAuth from '@/components/AppAuth.vue'
+import useUserStore from '@/stores/user'
+import { mapWritableState } from 'pinia'
+import { auth } from './includes/firebase'
 
 export default {
   components: {
     AppHeader,
     AppAuth
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ['userLoggedIn'])
+  },
+  created() {
+    if (auth.currentUser) {
+      this.userLoggedIn = true
+    }
   }
 }
 </script>
