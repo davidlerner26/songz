c667e79196dfc189b2b843d7687ca27863069311
davidlerner26 <david.lerner@hotmail.com>
2023-12-27 08:38:02 -0300

    Allow song edit

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -13,7 +13,13 @@
           </div>
           <div class="p-6">
             <!-- Composition Items -->
-            <composition-item v-for="song in songs" :key="song.docID" :song="song" />
+            <composition-item
+              v-for="(song, i) in songs"
+              :key="song.docID"
+              :song="song"
+              :updateSong="updateSong"
+              :index="i"
+            />
           </div>
         </div>
       </div>
@@ -48,6 +54,12 @@ export default {
 
       this.songs.push(song)
     })
+  },
+  methods: {
+    updateSong(i, values) {
+      this.songs[i].modified_name = values.modified_name
+      this.songs[i].genre = values.genre
+    }
   }
 }
 </script>
