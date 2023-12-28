de7b2e2948d7bebf40ccfd2e34734149100cbf62
davidlerner26 <david.lerner@hotmail.com>
2023-12-28 10:03:02 -0300

    Add transition on route change

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <!-- Header -->
   <app-header />
 
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
 
   <!-- Player -->
   <app-player></app-player>
@@ -35,3 +39,18 @@ export default {
   }
 }
 </script>
+
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
