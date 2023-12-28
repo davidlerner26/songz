66374c4cfa07af6b97d38dbbc4a570e71c5c2bca
davidlerner26 <david.lerner@hotmail.com>
2023-12-28 09:33:36 -0300

    Play audio, display duration and seek

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -8,6 +8,7 @@
     <div class="container mx-auto flex items-center">
       <!-- Play/Pause Button -->
       <button
+        @click.prevent="newSong(song)"
         type="button"
         class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
       >
@@ -84,8 +85,9 @@
 <script>
 import { auth, songsCollection, commentsCollection } from '@/includes/firebase'
 import { ErrorMessage } from 'vee-validate'
-import { mapState } from 'pinia'
+import { mapState, mapActions } from 'pinia'
 import useUserStore from '/stores/user'
+import usePlayerStore from '@/stores/player'
 
 export default {
   name: 'Song',
@@ -129,6 +131,7 @@ export default {
   },
   components: { ErrorMessage },
   methods: {
+    ...mapActions(usePlayerStore, ['newSong']),
     async addComment(values, { resetForm }) {
       this.comment_in_submission = true
       this.comment_show_alert = true
